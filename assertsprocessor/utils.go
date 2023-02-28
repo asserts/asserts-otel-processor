08c99bbef325ac205f9829f68009a2448a247589
jradhakrishnan <radhakrishnan.jankiraman@gmail.com>
2023-02-28 20:44:38 +0530

    Utils enhancements

--- assertsprocessor/utils.go
+++ assertsprocessor/utils.go
@@ -1,7 +1,92 @@
 package assertsprocessor
 
-import "go.opentelemetry.io/collector/pdata/ptrace"
+import (
+	"context"
+	"go.opentelemetry.io/collector/pdata/pcommon"
+	"go.opentelemetry.io/collector/pdata/ptrace"
+	conventions "go.opentelemetry.io/collector/semconv/v1.6.1"
+	"go.uber.org/zap"
+	"regexp"
+)
 
 func computeLatency(span ptrace.Span) float64 {
 	return float64(span.EndTimestamp()-span.StartTimestamp()) / 1e9
+}
+
+func compileRequestContextRegexps(logger *zap.Logger, config *Config) (*map[string]regexp.Regexp, error) {
+	logger.Info("consumer.Start compiling request context regexps")
+	var exps = map[string]regexp.Regexp{}
+	for attName, matchExpString := range *config.RequestContextExps {
+		compile, err := regexp.Compile(matchExpString)
+		if err != nil {
+			return nil, err
+		}
+		exps[attName] = *compile
+	}
+	logger.Debug("consumer.Start compiled request context regexps successfully")
+	return &exps, nil
+}
+
+func getExp(exps *map[string]regexp.Regexp, span ptrace.Span) string {
+	for attName, regExp := range *exps {
+		value, found := span.Attributes().Get(attName)
+		if found {
+			submatch := regExp.FindStringSubmatch(value.AsString())
+			if len(submatch) >= 1 {
+				return submatch[1]
+			}
+		}
+	}
+	return ""
+}
+
+func traceHasError(ctx context.Context, traces ptrace.Traces) bool {
+	var slice []int
+	callback := func(ns string, service string, ctx context.Context, traces ptrace.Traces, span ptrace.Span) error {
+		attributes := span.Attributes()
+		attributes.Range(func(k string, v pcommon.Value) bool {
+			if k == "error" && v.Bool() {
+				slice = append(slice, 1)
+				return false
+			} else {
+				return true
+			}
+		})
+		return nil
+	}
+	_ = spanIterator(ctx, traces, callback)
+	return len(slice) > 0
+}
+
+func spanIterator(ctx context.Context, traces ptrace.Traces,
+	callback func(string, string, context.Context, ptrace.Traces, ptrace.Span) error) error {
+	for i := 0; i < traces.ResourceSpans().Len(); i++ {
+		resourceSpans := traces.ResourceSpans().At(i)
+		resourceAttributes := resourceSpans.Resource().Attributes()
+
+		var namespace string
+		namespaceAttr, found := resourceAttributes.Get(conventions.AttributeServiceNamespace)
+		if found {
+			namespace = namespaceAttr.Str()
+		}
+
+		serviceAttr, found := resourceAttributes.Get(conventions.AttributeServiceName)
+		if !found {
+			continue
+		}
+		serviceName := serviceAttr.Str()
+		ilsSlice := resourceSpans.ScopeSpans()
+		for j := 0; j < ilsSlice.Len(); j++ {
+			ils := ilsSlice.At(j)
+			spans := ils.Spans()
+			for k := 0; k < spans.Len(); k++ {
+				span := spans.At(k)
+				err := callback(namespace, serviceName, ctx, traces, span)
+				if err != nil {
+					return err
+				}
+			}
+		}
+	}
+	return nil
 }
--- assertsprocessor/utils_test.go
+++ assertsprocessor/utils_test.go
@@ -1,8 +1,12 @@
 package assertsprocessor
 
 import (
+	"context"
 	"github.com/stretchr/testify/assert"
 	"go.opentelemetry.io/collector/pdata/ptrace"
+	conventions "go.opentelemetry.io/collector/semconv/v1.6.1"
+	"go.uber.org/zap"
+	"regexp"
 	"testing"
 )
 
@@ -11,4 +15,122 @@ func TestComputeLatency(t *testing.T) {
 	testSpan.SetStartTimestamp(1e9)
 	testSpan.SetEndTimestamp(1e9 + 4e8)
 	assert.Equal(t, 0.4, computeLatency(testSpan))
+}
+
+func TestCompileRequestContextRegexpsSuccess(t *testing.T) {
+	logger, _ := zap.NewProduction()
+	regexps, err := compileRequestContextRegexps(logger, &Config{
+		RequestContextExps: &map[string]string{
+			"attribute1": "Foo",
+			"attribute2": "Bar.+",
+		},
+	})
+	assert.Nil(t, err)
+	assert.NotNil(t, regexps)
+	assert.Equal(t, 2, len(*regexps))
+
+	regExp := (*regexps)["attribute1"]
+	assert.NotNil(t, regExp)
+	assert.True(t, regExp.MatchString("Foo"))
+
+	regExp = (*regexps)["attribute2"]
+	assert.NotNil(t, regExp)
+	assert.True(t, regExp.MatchString("Bart"))
+}
+
+func TestCompileRequestContextRegexpsFailure(t *testing.T) {
+	logger, _ := zap.NewProduction()
+	_, err := compileRequestContextRegexps(logger, &Config{
+		RequestContextExps: &map[string]string{
+			"attribute1": "+",
+			"attribute2": "Bar.+",
+		},
+	})
+	assert.NotNil(t, err)
+}
+
+func TestGetExpMatch(t *testing.T) {
+	testSpan := ptrace.NewSpan()
+	testSpan.Attributes().PutStr("http.url", "https://sqs.us-west-2.amazonaws.com/342994379019/NodeJSPerf-WithLayer")
+
+	compile, _ := regexp.Compile("https?://.+?(/.+?/.+)")
+	value := getExp(&map[string]regexp.Regexp{
+		"http.url": *compile,
+	}, testSpan)
+	assert.Equal(t, "/342994379019/NodeJSPerf-WithLayer", value)
+}
+
+func TestGetExpNoMatch(t *testing.T) {
+	testSpan := ptrace.NewSpan()
+	testSpan.Attributes().PutStr("http.url", "https://sqs.us-west-2.amazonaws.com/342994379019/NodeJSPerf-WithLayer")
+
+	compile, _ := regexp.Compile("https?://foo.+?(/.+?/.+)")
+	value := getExp(&map[string]regexp.Regexp{
+		"http.url": *compile,
+	}, testSpan)
+	assert.Equal(t, "", value)
+}
+
+func TestSpanIterator(t *testing.T) {
+	ctx := context.Background()
+	testTrace := ptrace.NewTraces()
+	resourceSpans := testTrace.ResourceSpans().AppendEmpty()
+	resourceSpans.Resource().Attributes().PutStr(conventions.AttributeServiceName, "api-server")
+	resourceSpans.Resource().Attributes().PutStr(conventions.AttributeServiceNamespace, "platform")
+	scopeSpans := resourceSpans.ScopeSpans().AppendEmpty()
+
+	testSpan := scopeSpans.Spans().AppendEmpty()
+
+	testSpan.SetSpanID([8]byte{1, 2, 3, 4, 5, 6, 7, 8})
+	testSpan.Attributes().PutStr("http.url", "https://sqs.us-west-2.amazonaws.com/342994379019/NodeJSPerf-WithLayer")
+
+	var expected = map[string]any{
+		"namespace": "platform", "service": "api-server", "ctx": ctx, "trace": testTrace, "span": testSpan,
+	}
+
+	var actual = map[string]any{}
+
+	err := spanIterator(ctx, testTrace, func(ns string, name string, ctx context.Context, trace ptrace.Traces, span ptrace.Span) error {
+		actual["namespace"] = ns
+		actual["service"] = name
+		actual["ctx"] = ctx
+		actual["trace"] = trace
+		actual["span"] = span
+		return nil
+	})
+	assert.Nil(t, err)
+	assert.Equal(t, expected, actual)
+}
+
+func TestTraceHasErrorFalse(t *testing.T) {
+	ctx := context.Background()
+	testTrace := ptrace.NewTraces()
+	resourceSpans := testTrace.ResourceSpans().AppendEmpty()
+	resourceSpans.Resource().Attributes().PutStr(conventions.AttributeServiceName, "api-server")
+	resourceSpans.Resource().Attributes().PutStr(conventions.AttributeServiceNamespace, "platform")
+	scopeSpans := resourceSpans.ScopeSpans().AppendEmpty()
+
+	testSpan := scopeSpans.Spans().AppendEmpty()
+
+	testSpan.SetSpanID([8]byte{1, 2, 3, 4, 5, 6, 7, 8})
+	testSpan.Attributes().PutStr("http.url", "https://sqs.us-west-2.amazonaws.com/342994379019/NodeJSPerf-WithLayer")
+
+	assert.False(t, traceHasError(ctx, testTrace))
+}
+
+func TestTraceHasErrorTrue(t *testing.T) {
+	ctx := context.Background()
+	testTrace := ptrace.NewTraces()
+	resourceSpans := testTrace.ResourceSpans().AppendEmpty()
+	resourceSpans.Resource().Attributes().PutStr(conventions.AttributeServiceName, "api-server")
+	resourceSpans.Resource().Attributes().PutStr(conventions.AttributeServiceNamespace, "platform")
+	scopeSpans := resourceSpans.ScopeSpans().AppendEmpty()
+
+	testSpan := scopeSpans.Spans().AppendEmpty()
+
+	testSpan.SetSpanID([8]byte{1, 2, 3, 4, 5, 6, 7, 8})
+	testSpan.Attributes().PutStr("http.url", "https://sqs.us-west-2.amazonaws.com/342994379019/NodeJSPerf-WithLayer")
+	testSpan.Attributes().PutBool("error", true)
+
+	assert.True(t, traceHasError(ctx, testTrace))
 }
